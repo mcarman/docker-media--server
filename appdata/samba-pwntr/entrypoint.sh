394c82c05b036cedb39361b319960e4e7ae1e624
mcarman <carman.md@live.com>
2023-07-22 06:13:55 -0600

    primary commit

---
+++ appdata/samba-pwntr/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# located in root
+
+set -e
+set -o pipefail
+
+handle_signal() {
+  PID=$!
+  echo "Received signal. PID is ${PID}"
+  kill -s SIGHUP $PID
+}
+
+trap "handle_signal" SIGINT SIGTERM SIGHUP
+
+/usr/sbin/smbd --foreground --no-process-group --debug-stdout & wait 
+
+exit $?
